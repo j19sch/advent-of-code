e106c4f9f979b66be2e5a08d5d8e48580efc294a
Joep Schuurkes <j19sch@gmail.com>
2019-12-02 18:27:32 +0100

    day 1 - puzzle 2

---
+++ day01/puzzle02.py
@@ -0,0 +1,19 @@
+import itertools
+
+# lessons learned:
+# - tuples are not sets
+# - print() is very expensive
+
+with open('./input-puzzle01.txt', 'r') as input_data:
+    result = 0
+    seen_results = {result}
+
+    for line in itertools.cycle(input_data.readlines()):
+        result += int(line)
+        if result in seen_results:
+            first_seen_before = result
+            break
+        else:
+            seen_results.add(result)
+
+    print(f'==> the answer: {first_seen_before}')
