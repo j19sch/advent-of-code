f9cf91ea25d0d00766d5cd3332695981c495e2e9
Joep Schuurkes <joep.schuurkes@mendix.com>
2019-12-09 18:17:09 +0100

    WIP: adds non-entirely working solution for day 05 puzzle 2

--- 2019/day05-puzzle2.py
+++ 2019/day05-puzzle2.py
@@ -24,10 +24,14 @@ def parse_instruction(intcode_program,read_pos):
     return opcode, mode_params
 
 with open(filename, 'r') as input_file:
-    intcode_program = [int(_) for _ in input_file.read().split(',')]
+    # intcode_program = [int(_) for _ in input_file.read().split(',')]
+
+    inp_string = "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9"
+    intcode_program = [int(_) for _ in inp_string.split(',')]
+
     read_pos = 0
 
-    input_instruction = 1
+    input_instruction = 0
     step = 0
 
     while True:
@@ -38,30 +42,42 @@ with open(filename, 'r') as input_file:
         if opcode == 99:
             break
 
-        if opcode == 1:
+        if opcode == 1:  # addition
             input_a_loc, input_b_loc, output_loc = get_input_parameters(3, intcode_program, read_pos, mode_params)
             result = intcode_program[input_a_loc] + intcode_program[input_b_loc]
             intcode_program[output_loc] = result
             read_pos += 4
-        elif opcode == 2:
+        elif opcode == 2:  # multiplication
             input_a_loc, input_b_loc, output_loc = get_input_parameters(3, intcode_program, read_pos, mode_params)
             result = intcode_program[input_a_loc] * intcode_program[input_b_loc]
             intcode_program[output_loc] = result
             read_pos += 4
-        elif opcode == 3:
+        elif opcode == 3:  # input
             input_instruction_loc, = get_input_parameters(1, intcode_program, read_pos, mode_params)
             intcode_program[input_instruction_loc] = input_instruction
             read_pos += 2
-        elif opcode == 4:
+        elif opcode == 4:  # output
             output_loc, = get_input_parameters(1, intcode_program, read_pos, mode_params)
             output = intcode_program[output_loc]
             print(f"output: {output}")
             read_pos += 2
-        elif opcode == 5:
-            pass
-        elif opcode == 6:
-            pass
-        elif opcode == 7:
-            pass
-        elif opcode == 8:
-            pass
+        elif opcode == 5:  # jump-if-true
+            check, value_loc = get_input_parameters(2, intcode_program, read_pos, mode_params)
+            if check != 0:
+                read_pos = intcode_program[value_loc]
+            else:
+                read_pos += 3
+        elif opcode == 6:  # jump-if-fale
+            check, value_loc = get_input_parameters(2, intcode_program, read_pos, mode_params)
+            if check == 0:
+                read_pos = intcode_program[value_loc]
+            else:
+                read_pos += 3
+        elif opcode == 7:  # less than
+            val1_loc, val2_loc, output_loc = get_input_parameters(3, intcode_program, read_pos, mode_params)
+            intcode_program[output_loc] = 1 if intcode_program[val1_loc] < intcode_program[val2_loc] else 0
+            read_pos += 4
+        elif opcode == 8:  # equals
+            val1_loc, val2_loc, output_loc = get_input_parameters(3, intcode_program, read_pos, mode_params)
+            intcode_program[output_loc] = 1 if intcode_program[val1_loc] == intcode_program[val2_loc] else 0
+            read_pos += 4
